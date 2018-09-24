d2f1a46cddaf95683efc847ee6dbd806a3dc5237
Jose Eduardo Paes <j.eduardopaes@gmail.com>
2018-09-24 02:32:45 -0300

    Utiliza url para verificar a requisição e verificar o path solicitado na requisição.

--- index.js
+++ index.js
@@ -4,10 +4,22 @@
 
 //dependências
 const http = require('http');
+const url = require('url');
 
 //O servidor deve responder a todos os requestes com uma String.
 const server = http.createServer(function (req, res){
+
+  //Pega a requisição para URL e parse 
+  let parsedUrl = url.parse(req.url,true);
+  //Pega o Path(caminho)
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g,"");
+
+  //Envia uma resposta
   res.end('Oi Web\n');
+
+  //Log o Path requisitado
+  console.log('Requisição para o caminho: '+trimmedPath+'\n');
 });
 
 //Inicializa o servidor que deve escutar a porta 3000.
