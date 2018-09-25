441ac2aa4abe44cf1132c81c4efc9201d4872474
Jose Eduardo Paes <j.eduardopaes@gmail.com>
2018-09-25 20:36:33 -0300

    Criando rotas especificas para a API.

--- index.js
+++ index.js
@@ -34,13 +34,39 @@ const server = http.createServer(function (req, res){
   req.on('end',function(){
     buffer += decoder.end();
 
-    //Envia uma resposta
-    res.end('Oi Web\n');
+    //Escolhe o handler que o request deve ir. Se não foi encontrado nenhum handler usar o notFound.
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-    //Log o Path requisitado
-    console.log('Requisição de: '+method+' para o caminho: '+trimmedPath+' com os parametros: ',queryStringObject);
-    console.log('Headers do request: ',headers);
-    console.log('Payload: ',buffer);
+    // Constroi os dados(data) object para enviar ao handler
+    var data = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
+
+    //Roteia o request para o handler especifico
+    chosenHandler(data, function(statusCode, payload){
+      // Utilizar o status code do handler ou o default(padrão) 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      // Utilizar o payload do handler ou default(padrão) do callback
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Converte payload to string
+      var payloadString = JSON.stringify(payload);
+
+      //Envia resposta
+      res.writeHead(statusCode);
+
+      res.end(payloadString);
+
+      //Log o Path requisitado
+      console.log('Requisição de: '+method+' para o caminho: '+trimmedPath+' com os parametros: ',queryStringObject);
+      console.log('Headers do request: ',headers);
+      console.log('Payload: ',buffer);
+      console.log('Resposta enviada: ',statusCode, payload);
+    });
 
   });
 
@@ -50,3 +76,22 @@ const server = http.createServer(function (req, res){
 server.listen(3000,function(){
   console.log('Servidor escutando porta 3000...');
 });
+
+// Define o handler
+var handlers = {}
+
+handlers.sample = function(data,callback){
+  // Callback uma http status code, e um payload object
+  callback(406,{'name':'sample handler'});
+};
+
+// Not Found handler
+
+handlers.notFound = function(data,callback){
+  callback(404);
+};
+
+// Define um request router
+var router = {
+  'sample' : handlers.sample
+};
